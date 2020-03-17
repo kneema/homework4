9d4c570dbd3c8a52d1966be32c6436cda57cb7c0
Nima Sherpa <ndsherpa@gmail.com>
2020-03-17 17:44:14 +1030

    Updated style.css, app.js, and index.html

--- app.js
+++ app.js
@@ -1,68 +1,78 @@
 //select HTML elements and store references to them in variables
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const qImg = document.getElementById("qImg");
+const ChA = document.getElementById("A");
+const ChB = document.getElementById("B");
+const ChC = document.getElementById("C");
+const counter = document.getElementById("counter");
+const progress = document.getElementById("progress");
+const scoreDiv = document.getElementById("scoreContainer");
 const submitButton = document.getElementById('submit');
-var timerSeconds = document.querySelector(".seconds");
-const myQuestions = [
+const resultsContainer = document.getElementById('results');
+
+let questions = [
     {
       question: "Who invented JavaScript?",
       answers: {
-        a: "Douglas Crockford",
-        b: "Sheryl Sandberg",
-        c: "Brendan Eich"
+        ChA: "Douglas Crockford",
+        ChB: "Sheryl Sandberg",
+        ChC: "Brendan Eich"
       },
-      correctAnswer: "c"
+      correct: "C"
     },
     {
       question: "Which one of these is a JavaScript package manager?",
       answers: {
-        a: "Node.js",
-        b: "TypeScript",
-        c: "npm"
+        ChA: "Node.js",
+        ChB: "TypeScript",
+        ChC: "npm"
       },
-      correctAnswer: "c"
+      correct: "C"
     },
     {
       question: "Which tool can you use to ensure code quality?",
       answers: {
-        a: "Angular",
-        b: "jQuery",
-        c: "RequireJS",
-        d: "ESLint"
+        ChA: "Angular",
+        ChB: "jQuery",
+        ChC: "ESLint"
       },
-      correctAnswer: "d"
+      correct: "C"
     },
     {
-        question: "What are variables used for in JavaScript?",
-        answers: {
-          a: "For changing language settings",
-          b: "For storing or holding data",
-          c: "For changing a value's data type",
+      question: "What are variables used for in JavaScript?",
+      answers: {
+          ChA: "For changing language settings",
+          ChB: "For storing or holding data",
+          ChC: "For changing a value's data type"
       },
-      correctAnswer: "b"
+      correct: "B"
     },
     {
-        question: "What is the correct way to call the random method on the Math global object?",
-        answers: {
-          a: "random.Math()",
-          b: "Math(randon)",
-          c: "Math.random()",
-          d: "math.random()",
+      question: "What is the correct way to call the random method on the Math global object?",
+      answers: {
+          ChA: "random.Math()",
+          ChB: "Math(randon)",
+          ChC: "Math.random()"
       },
-      correctAnswer: "c"
-    }
+      correct: "C"
+    } 
     
   ];
 
-//quiz functions to build quiz and show results
-function buildQuiz(){
+// creating some variables
 
-    // variable to store the HTML output
-  const output = [];
+const lastQuestion = (questions.length - 1);
+let runningQuestion = 0;
+let timerSeconds = 10;
+const questionTime = 10; // 10s
+let TIMER;
+let score = 0;
 
-  // for each question...
-  myQuestions.forEach(
-    (currentQuestion, questionNumber) => {
+// for each question...
+questions.forEach(
+(currentQuestion, questionNumber) => {
 
       // variable to store the list of possible answers
       const answers = [];
@@ -85,13 +95,96 @@ function buildQuiz(){
         `<div class="question"> ${currentQuestion.question} </div>
         <div class="answers"> ${answers.join('')} </div>`
       );
-    }
-  );
+    });
 
   // Combine our output list into one string of HTML and put it on the page
-  quizContainer.innerHTML = output.join('');
+  quiz.innerHTML = output.join('');
+
+//render question
+function renderQuestion(){
+    question.innerHTML = "<p>"+ q.question +"</p>";
+    ChA.innerHTML = q.ChA;
+    ChB.innerHTML = q.ChB;
+    ChC.innerHTML = q.ChC;
 }
 
+// start quiz
+function startQuiz(){
+    start.style.display = "none";
+    renderQuestion();
+    quiz.style.display = "block";
+    renderProgress();
+    renderCounter();
+    TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
+}
+start.addEventListener("click",startQuiz);
+
+// render progress
+function renderProgress(){
+    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
+        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
+    }
+}
+
+// counter render
+
+function renderCounter(){
+    if(count <= questionTime){
+        counter.innerHTML = count;
+        timeGauge.style.width = count * gaugeUnit + "px";
+        count++
+    }else{
+        count = 0;
+        // change progress color to red
+        answerIsWrong();
+        if(runningQuestion < lastQuestion){
+            runningQuestion++;
+            renderQuestion();
+        }else{
+            // end the quiz and show the score
+            clearInterval(TIMER);
+            scoreRender();
+        }
+    }
+}
+
+// checkAnswer
+
+function checkAnswer(answer){
+    if( answer == questions[runningQuestion].correct){
+        // answer is correct
+        score++;
+        // change progress color to green
+        answerIsCorrect();
+    }else{
+        // answer is wrong
+        // change progress color to red
+        answerIsWrong();
+    }
+    count = 0;
+    if(runningQuestion < lastQuestion){
+        runningQuestion++;
+        renderQuestion();
+    }else{
+        // end the quiz and show the score
+        clearInterval(TIMER);
+        scoreRender();
+    }
+}
+
+// if answer is correct
+if(userAnswer === currentQuestion.correct){
+    // add to the number of correct answers
+    numCorrect++;
+
+    // color the answers green
+    answerContainers[questionNumber].style.color = 'lightgreen';
+  }
+  // if answer is wrong or blank
+  else{
+    // color the answers red
+    answerContainers[questionNumber].style.color = 'red';
+  }
 //Show results of the quiz
 function showResults(){
     // gather answer containers from our quiz
@@ -101,35 +194,34 @@ function showResults(){
   let numCorrect = 0;
 
   // for each question...
-  myQuestions.forEach( (currentQuestion, questionNumber) => {
+  questions.forEach( (currentQuestion, questionNumber) => {
 
     // find selected answer
     const answerContainer = answerContainers[questionNumber];
     const selector = `input[name=question${questionNumber}]:checked`;
     const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-    // if answer is correct
-    if(userAnswer === currentQuestion.correctAnswer){
-      // add to the number of correct answers
-      numCorrect++;
-
-      // color the answers green
-      answerContainers[questionNumber].style.color = 'lightgreen';
-    }
-    // if answer is wrong or blank
-    else{
-      // color the answers red
-      answerContainers[questionNumber].style.color = 'red';
-    }
-  });
-
-  // show number of correct answers out of total
-  resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-
-}
-
-// display quiz right away
-buildQuiz();
-
+}  
 // on submit, show results
-submitButton.addEventListener('click', showResults);
+ submitButton.addEventListener('click', showResults);
+
+// show number of correct answers out of total
+resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+
+// score render
+function scoreRender(){
+    scoreDiv.style.display = "block";
+    
+    // calculate the amount of question percent answered by the user
+    const scorePerCent = Math.round(100 * score/questions.length);
+    
+    // choose the image based on the scorePerCent
+    let img = (scorePerCent >= 80) ? "img/5.png" :
+              (scorePerCent >= 60) ? "img/4.png" :
+              (scorePerCent >= 40) ? "img/3.png" :
+              (scorePerCent >= 20) ? "img/2.png" :
+              "img/1.png";
+    
+    scoreDiv.innerHTML = "<img src="+ img +">";
+    scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
+});
